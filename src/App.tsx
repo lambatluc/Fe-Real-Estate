bd6022d1870430beb8acc34749a14ad4a8f63b7a
lambatluc <anhvu731923@gmail.com>
2024-07-15 17:49:33 +0700

    Login Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Login from "./features/Login/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./features/Home/Home";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
